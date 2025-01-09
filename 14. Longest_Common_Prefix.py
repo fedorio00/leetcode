58541dbe175ae3fde859ddeb784d7607e71967b9
fedorio00 <159474500+fedorio00@users.noreply.github.com>
2025-01-09 14:44:34 +0300

    14. Longest_Common_Prefix

---
+++ 14. Longest_Common_Prefix.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        answer = ""
+        n = len(strs)
+        min_word = min(strs, key=len)
+        for i in range(len(min_word)):
+            for j in strs:
+                if j[i] != min_word[i]:
+                    return answer
+            answer += min_word[i]
+        return answer
+                
+test0 = ["flower","flow","flight"]
+test1 = ["dog","racecar","car"]
+print(Solution().longestCommonPrefix(test0))
+print(Solution().longestCommonPrefix(test1))
